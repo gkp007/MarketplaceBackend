5f3c6c60c6b93cad4d4569d7f2f0e95393f7afab
Debabrata Giri <debabratagiri@email.com>
2024-11-08 15:59:20 +0530

    handel utils

---
+++ backend/utils/jwtToken.ts
@@ -0,0 +1,26 @@
+import { Response } from 'express';
+
+interface User {
+    getJWTToken: () => string;
+}
+
+const sendToken = (user: User, statusCode: number, res: Response): void => {
+    const token = user.getJWTToken();
+
+    try {
+        const option = {
+            expires: new Date(Date.now() + Number(process.env.COOKIE_EXPIER) * 24 * 60 * 60 * 1000),
+            httpOnly: true
+        };
+        res.status(statusCode).cookie('token', token, option).json({
+            success: true,
+            user,
+            token
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ success: false, error });
+    }
+};
+
+export default sendToken;
